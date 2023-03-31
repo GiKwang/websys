8ecf85a596eb6a264f87096a2dbeb4014f0c5423
GiKwang <Chonggrk@yahoo.com.sg>
2023-03-31 21:43:15 +0800

    update process_logout

--- process_logout.php
+++ process_logout.php
@@ -1,8 +1,9 @@
 <?php
-
 session_start();
+
 // Unset all session variables
 $_SESSION = array();
+
 // Unset the email session variable
 unset($_SESSION['email']);
 unset($_SESSION['emailcheck']);
@@ -12,9 +13,17 @@ if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 }
+
+// Destroy the remember_email cookie
+if (isset($_COOKIE['remember_email'])) {
+    unset($_COOKIE['remember_email']);
+    setcookie('remember_email', '', time() - 3600, '/');
+}
+
 // Destroy the session
 session_destroy();
+
 // Redirect to the logout page
 header('Location: logout.php');
 exit;
-
+?>
